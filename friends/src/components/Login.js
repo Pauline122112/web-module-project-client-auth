4c95458f33136e0e507decb219ada2252ecf4b86
Pauline122112 <pauline.design007@gmail.com>
2021-08-17 20:29:21 -0500

    made changes to login js

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,26 +1,63 @@
 import React from "react";
 import axios from "axios";
-import { render } from "sass";
+
 
 class Login extends React.Component {
+    state = {
+        credentials: {
+            username: '',
+            password: ''
+        }
+    }
 
 
 
-
-handleChange = err => {
-
+handleChange = e => {
+this.setState({
+	credentials: {
+		...this.state.credentials,
+		[e.target.name]: e.target.value,
+	},
+});
 }
 
 
-login = err => {
+login = e=> {
+    e.preventDefault()
+     axios
+				.post("http://localhost:5000/api/login", this.state.credentials)
+				.then((res) => {
+					console.log(res.data.payload);
+					localStorage.setItem("token", res.data.token);
+					this.props.history.push("/protected");
+				})
+				.catch((err) => {
+					console.log(err);
+				});
 
 }
 
 
 render() {
     return (
-
-    )
+			<div>
+				<form onSubmit={this.login}>
+					<input
+						type="text"
+						name="username"
+						value={this.state.credentials.username}
+						onChange={this.handleChange}
+					/>
+					<input
+						type="password"
+						name="password"
+						value={this.state.credentials.password}
+						onChange={this.handleChange}
+					/>
+					<button>Log in</button>
+				</form>
+			</div>
+		);
 }
 
 }
