9ad68bea215b8dc2b7e72a292b6f30bfbf226b04
Pauline122112 <pauline.design007@gmail.com>
2021-08-17 19:58:37 -0500

    setting up login and logout

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import Logout from "./components/Logout";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div className="App">
+				<ul>
+					<li>
+						<Link to="/login">Login</Link>
+					</li>
+					<li>
+						<Link to="/logout">Logout</Link>
+					</li>
+				</ul>
+
+				<Switch>
+					<Route path="/logout" component={Logout} />
+					<Route path="/login" component={Login} />
+					<Route path="/" component={Login} />
+				</Switch>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
---
+++ friends/src/components/Login.js
@@ -0,0 +1,28 @@
+import React from "react";
+import axios from "axios";
+import { render } from "sass";
+
+class Login extends React.Component {
+
+
+
+
+handleChange = err => {
+
+}
+
+
+login = err => {
+
+}
+
+
+render() {
+    return (
+
+    )
+}
+
+}
+
+export default Login
