76d3b5cd86e4b69e39101afb05a7201c4b08bc9b
Pauline122112 <pauline.design007@gmail.com>
2021-08-17 21:46:38 -0500

    starting on friendsList

--- friends/src/App.js
+++ friends/src/App.js
@@ -15,11 +15,15 @@ function App() {
 					<li>
 						<Link to="/logout">Logout</Link>
 					</li>
+					<li>
+						<Link to="/friends">Friends</Link>
+					</li>
 				</ul>
 
 				<Switch>
 					<Route path="/logout" component={Logout} />
 					<Route path="/login" component={Login} />
+					<Route path="/friends" component={Login} />
 					<Route path="/" component={Login} />
 				</Switch>
 			</div>
---
+++ friends/src/components/FriendList.js
@@ -0,0 +1,36 @@
+import React from "react";
+import moment from "moment";
+import Loader from "react-loader-spinner";
+import axiosWithAuth from "./../utils/axiosWithAuth";
+
+
+class Friends extends React.Component {
+
+    state = {
+        friends: []
+    }
+
+    componentDidMount() {
+
+    }
+
+    getData = () => {
+
+    }
+
+    formatData = () => {
+
+    }
+
+    render() {
+        const friends = this.formatData()
+        return (
+            
+        )
+
+    }
+
+
+}
+
+export default Friends
